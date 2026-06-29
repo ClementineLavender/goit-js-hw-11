ae7d66e804c0c82a7dc3d8a73e60cd19a14084a3
ClementineLavender <natalibulavina78@gmail.com>
2026-06-29 23:15:58 +0200

    fix resolve module specifiers and close brackets

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import iziToast from 'izitoast';
 import 'izitoast/dist/css/iziToast.min.css';
+
 import { getImagesByQuery } from './js/pixabay-api.js';
 import {
   createGallery,
@@ -8,49 +9,51 @@ import {
   hideLoader,
 } from './js/render-functions.js';
 
-const searchForm = document.querySelector('.search-form');
+const searchForm = document.querySelector('.form');
 
-searchForm.addEventListener('submit', event => {
-  event.preventDefault();
+if (searchForm) {
+  searchForm.addEventListener('submit', event => {
+    event.preventDefault();
 
-  const form = event.currentTarget;
-  const searchQuery = form.elements['search-text'].value.trim();
+    const form = event.currentTarget;
+    const searchQuery = form.elements['search-text'].value.trim();
 
-  if (searchQuery === '') {
-    iziToast.warning({
-      title: 'Warning',
-      message: 'Please enter a search query!',
-      position: 'topRight',
-    });
-    return;
-  }
-
-  clearGallery();
-  showLoader();
-
-  getImagesByQuery(searchQuery)
-    .then(data => {
-      if (data.hits.length === 0) {
-        iziToast.error({
-          message:
-            'Sorry, there are no images matching your search query. Please try again!',
-          position: 'topRight',
-        });
-        return;
-      }
-
-      createGallery(data.hits);
-    })
-    .catch(error => {
-      console.error(error);
-      iziToast.error({
-        title: 'Error',
-        message: 'Something went wrong. Please try again later.',
+    if (searchQuery === '') {
+      iziToast.warning({
+        title: 'Warning',
+        message: 'Please enter a search query!',
         position: 'topRight',
       });
-    })
-    .finally(() => {
-      hideLoader();
-      form.reset();
-    });
-});
+      return;
+    }
+
+    clearGallery();
+    showLoader();
+
+    getImagesByQuery(searchQuery)
+      .then(data => {
+        if (data.hits.length === 0) {
+          iziToast.error({
+            message:
+              'Sorry, there are no images matching your search query. Please try again!',
+            position: 'topRight',
+          });
+          return;
+        }
+
+        createGallery(data.hits);
+      })
+      .catch(error => {
+        console.error(error);
+        iziToast.error({
+          title: 'Error',
+          message: 'Something went wrong. Please try again later.',
+          position: 'topRight',
+        });
+      })
+      .finally(() => {
+        hideLoader();
+        form.reset();
+      });
+  });
+}
